ebfb6ec19b5556f4a0a9d41087df21646db70c5b
s3769969 <s3769969@student.rmit.edu.au>
2019-10-12 21:22:27 +1100

    added session verification and redirect

--- a4/receipt.php
+++ a4/receipt.php
@@ -1,11 +1,18 @@
 <?php
+session_start();
+
+//alert($_SESSION['cart']['cust']['expiry']);
+
+function alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
 
 if(empty($_SESSION))
 {
-  //header("Cache-Control: no-cache, must-revalidate");
-  //header("Expires: date("d-m-Y")");
-  header("Location: test.php");
-  exit;
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Expires: date(\"Y-m-d\")");
+  header("Location: index.php");
+  exit();
 }else{
   //add order to spreadsheet and print receipts
   echo "Welcome";
--- a4/tools.php
+++ a4/tools.php
@@ -31,5 +31,3 @@ function php2js( $arr, $arrName ) {
 
 
 ?>
-
-<?php ?>
